038fc4521036086eed2c4651dbe0473738d2be2e
HassanShanJava <hassanshan675@gmail.com>
2022-12-05 23:30:20 +0500

    setup api, workking on uni and env next

--- Back-End/src/index.ts
+++ Back-End/src/index.ts
@@ -11,15 +11,39 @@ const PORT=5000;
 // successfully made api to send msg
 
 //2nd Api
-app.get("/hello", (req: Request, res: Response) => {
+
+
+// now building api for resource with crud operatioons
+// app.get("/decks", (req: Request, res: Response) => {
+//   // res.set
+//   res.send("Hello world");
+// });
+
+app.use(express.json()) //express middleware function for support
+
+
+// use post to createbrand new resoucre
+app.post("/decks", async(req: Request, res: Response) => {
   // res.set
-  res.send("Hello world");
+  // res.send("Hello world");
+  
+  // we create new model and add it to db
+  console.log(req.body);
+  
+
+  const newDeck=new   Deck({
+    title:req.body.title
+  }) //oop
+
+  // now its saved in db
+  const createdDeck= await  newDeck.save() //we get uniqe id in mongo collection
+  res.json(createdDeck)
 });
 
 // 1st api
-app.get("/", (req: Request, res: Response) => {
-  res.send("Sup Bro :)");
-});
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Sup Bro :)");
+// });
 
 // this is a promise
 // so its some asynch code
