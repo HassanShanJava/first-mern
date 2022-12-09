78f10e01873c782bf4c22bc49946b5ca243b3159
HassanShanJava <hassanshan675@gmail.com>
2022-12-09 11:31:44 +0500

    api end point integrateed

--- Back-End/src/index.ts
+++ Back-End/src/index.ts
@@ -28,11 +28,32 @@ const PORT=5000;
 // });
 
 
+
+//everytime to mke an api, a set of headers go to the backend
+
+
+
 app.use(cors({
   origin:"*",  //anyone can access
 }))
 app.use(express.json()) //express middleware function for support
 
+
+// new  endpoint
+
+// when fetch data-->get requeust
+// when push data-->post requeust
+app.get("/decks", async(req: Request, res: Response)=>{
+
+  // to fetch all decks & send  to user
+  // 1. detahdata from mongo
+  const deck= await Deck.find() //you can also add reex and be more specific here 
+  console.log(deck); //change thunderbt to get method, it crashes but youu can see the console log
+  
+  // 2. send data to user in array
+  res.json(deck) //now fiure out, how to fetch it from the front end
+}
+)
 
 // use post to createbrand new resoucre
 app.post("/decks", async(req: Request, res: Response) => {
--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,24 +1,78 @@
 // import { title } from "process";
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 import "./App.css"
+
+
 
 const App: React.FC=()=> {
   const [title, setTitle]=useState('')
+  
+  // const [title, setTitle]=useState('')[0]for the setValue ===> title
+  // const [title, setTitle]=useState('')[1]for the dispacther ===> setTitle
 
-  const handlerCreateDeck=(e:React.FormEvent)=>{
+  const [decks, setDecks]=useState<any>([])
+  
+  // when the app.tsx mounts(loads) we want to trigger an api request
+  useEffect(()=>{
+
+
+    
+    async function fetchData(){
+      const response=await fetch("http://localhost:5000/decks")
+      const newDecks=await response.json()
+      setDecks(newDecks)
+      
+    }
+    fetchData();  //now u need to store this data, we need a hok that stores an array of this dzta 
+
+    
+    // OR
+    // (async ()=>{
+    //   await fetch("http://localhost:5000/decks")
+    // })(); self-calling fn
+
+
+
+    // here comes callback
+    // console.log("we are here ");
+
+    // // clean up function
+    // return ()=>{
+    //   console.log("cleanup");
+    // }
+    
+  
+  },[]) //here is dependicy arrgument, if empty, only works when it mounts and unmounts(cleansup)
+
+
+
+  const handlerCreateDeck= async (e:React.FormEvent)=>{
     e.preventDefault()
     // we need to talk too backend api to preset this deck
-    fetch("http://localhost:5000/decks",{
+  await fetch("http://localhost:5000/decks",{
       method:"POST",
       body:JSON.stringify({
         title,
-      })
+      }),
+      headers:{
+        "Content-Type":"application/json",
+      }
     })
+    setTitle("")
   }
   
 
   return (
     <div className="App">
+
+      <ul className="decks">
+        {
+          decks.map((deck:any)=>(
+            <li >{deck.title}</li>
+          ))
+        }
+      </ul>
+
       <form onSubmit={handlerCreateDeck}>
         <label htmlFor="deck-title">Deck Title</label>
         <input
